de36345423e68ec4a583d89ab02ae5fb5be7a8e5
SafiyeM <122940752+SafiyeM@users.noreply.github.com>
2023-02-27 19:24:31 +0100

    Java_Programming Day09

--- src/day09_scanner/ScannerIntro3.java
+++ src/day09_scanner/ScannerIntro3.java
@@ -1,4 +1,19 @@
 package day09_scanner;
 
+import java.util.Scanner;
+
 public class ScannerIntro3 {
+
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Would you like to continue?");
+        String answer= input.next();
+
+        System.out.println("You have entered : "+answer);
+
+
+
+    }
 }
