cba707f5e7620893b9a61dec557a7f2c8e4f807d
SafiyeM <122940752+SafiyeM@users.noreply.github.com>
2023-02-28 08:17:15 +0100

    Java_Programming Day09

--- src/day09_scanner/ScannerIntro3.java
+++ src/day09_scanner/ScannerIntro3.java
@@ -8,8 +8,14 @@ public class ScannerIntro3 {
 
         Scanner input= new Scanner(System.in);
 
+        System.out.println("Enter a character");
+        char ch= input.next().charAt(0);
+
+        System.out.println("You have entered: " +ch);
+
         System.out.println("Would you like to continue?");
-        String answer= input.next();
+        String answer= input.next();   // It returns only until space
+        //String answer= input.nextLine(); // It returns entire Line --> It causes an issue if we use with other methods
 
         System.out.println("You have entered : "+answer);
 
