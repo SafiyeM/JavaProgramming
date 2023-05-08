848ca287c999e606258e9001bff1e121b59cd15d
SafiyeM <122940752+SafiyeM@users.noreply.github.com>
2023-05-08 08:40:45 +0200

    added day34

---
+++ src/java_programming_lessons/day34_abstraction/Airplane.java
@@ -0,0 +1,10 @@
+package java_programming_lessons.day34_abstraction;
+
+import java_programming_lessons.day34_abstraction.animalTask.Flyable;
+
+public class Airplane implements Flyable {
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ src/java_programming_lessons/day34_abstraction/Person.java
@@ -0,0 +1,16 @@
+package java_programming_lessons.day34_abstraction;
+
+import java_programming_lessons.day34_abstraction.animalTask.Playable;
+import java_programming_lessons.day34_abstraction.animalTask.Swimmable;
+
+public class Person implements Swimmable, Playable {
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
