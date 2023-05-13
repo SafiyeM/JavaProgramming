295822d8a2dd6619c51fe8d9e060f574ab02fd9f
SafiyeM <122940752+SafiyeM@users.noreply.github.com>
2023-05-13 15:06:56 +0200

    initial commit

--- src/java_programming_lessons/day35_polymorphism/transportation/Tesla.java
+++ src/java_programming_lessons/day35_polymorphism/transportation/Tesla.java
@@ -1,6 +1,9 @@
 package java_programming_lessons.day35_polymorphism.transportation;
 
-public class Tesla implements AutoPilot, Electric {
+public class Tesla extends Car implements AutoPilot, Electric {
+    public Tesla(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
 
     // to implement AutoPilot is enough because AutoPark is Parent of AutoPilot, so Tesla can inherits both
 
@@ -16,6 +19,16 @@ public class Tesla implements AutoPilot, Electric {
 
     @Override
     public void charge() {
+
+    }
+
+    @Override
+    public void transportPeople() {
+
+    }
+
+    @Override
+    public void start() {
 
     }
 }
