961ee42a95dd7a829ee706b352b98d2852df08e5
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-05-22 14:45:37 +0200

    added new changes

--- src/java_practice/day15_whileLoop/DivideTwoNumbers.java
+++ src/java_practice/day15_whileLoop/DivideTwoNumbers.java
@@ -3,16 +3,24 @@ package java_practice.day15_whileLoop;
 public class DivideTwoNumbers {
     public static void main(String[] args) {
 
+        int a = 50;
+        int b = 9;
 
+        int count = 0;
 
+        while (a >= b) {
+            a -= b;
+            count++;
+        }
 
-
-
-
+        System.out.println(count + " with a remainder of " + a);
 
 
     }
+
+
 }
+
 /*
 4.  Create a class named DivideTwoNumbers and Write a program that can divide
     two positive numbers without using / (division) and * (multiplication) operators.
